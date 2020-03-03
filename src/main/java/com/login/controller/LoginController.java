91b5a90ef96748339a35692f47939c86aa9cd2e2
LeeJiHyeong <easybrother0103@gmail.com>
2020-03-03 16:05:00 +0900

    add LoginController.java

---
+++ src/main/java/com/login/controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.login.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+	
+	@RequestMapping(value = "login")
+	public String doLogin() {
+		
+		return "";
+	}
+}
