80cca8cc839681267c61b352c29565b7b2eeaad4
kihwankim <clack2933@gmail.com>
2020-06-23 03:20:25 +0900

    add domains

--- src/main/java/com/example/demo/login/domain/User.java
+++ src/main/java/com/example/demo/login/domain/User.java
@@ -1,23 +1,57 @@
 package com.example.demo.login.domain;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.NaturalId;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Set;
 
-@Data
+@Getter
+@Setter
 @Entity
-@Table(name = "User")
-public class User {
+@Table(name = "user", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {
+                "username"
+        }),
+})
+public class User extends DateAudit { // date type extends 하기
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
-    @Column(name = "user_name")
+
+    @NotBlank
+    @NaturalId
+    @Size(max = 10)
+    @Column(name = "username")
     private String userName;
+
+    @NotBlank
     @Column(name = "password")
+    @Size(max = 50)
     private String password;
+
+    @Column(name = "name")
+    private String name;
+
     @Column(name = "active")
     private boolean active;
-    @Column(name = "roles")
-    private String roles;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+
+    public User(String name, String username, String password) {
+        this.name = name;
+        this.userName = username;
+        this.password = password;
+    }
+
+    public User() {
+    }
 }
