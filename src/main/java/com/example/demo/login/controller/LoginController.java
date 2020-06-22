1027355bd67291ec52ed9ed56b48bd78082bd7eb
LeeJiHyeong <mky1168@naver.com>
2020-06-22 15:57:59 +0900

    add login test page

--- src/main/java/com/example/demo/login/controller/LoginController.java
+++ src/main/java/com/example/demo/login/controller/LoginController.java
@@ -1,14 +1,32 @@
 package com.example.demo.login.controller;
 
+import java.lang.ProcessBuilder.Redirect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 	
-	@RequestMapping(value = "login")
-	public String doLogin() {
+	private Logger logger = LoggerFactory.getLogger(LoginController.class);
+	
+	@RequestMapping(value = "login_test")
+	public String goTestLogin() {
 		
-		return "";
+		return "login_test.html";
+	}
+	
+	@RequestMapping(value = "do_login_test", method=RequestMethod.POST)
+	public String doTestLogin(@RequestParam("user_id") String userId, @RequestParam("user_password") String userPwd) {
+
+		logger.info("*** test id : " + userId);
+		logger.info("*** test pwd : " + userPwd);
+		
+		return "login_test.html";
 	}
 }
