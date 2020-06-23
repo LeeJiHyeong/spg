b0e47e16df0d06ca1b9c373a915ba552ffa3dbd2
kihwankim <clack2933@gmail.com>
2020-06-24 01:11:25 +0900

    delete some codes which we don't use

--- src/main/java/com/example/demo/login/controller/LoginController.java
+++ src/main/java/com/example/demo/login/controller/LoginController.java
@@ -1,33 +1,33 @@
 package com.example.demo.login.controller;
 
-import java.lang.ProcessBuilder.Redirect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
-	
-	private Logger logger = LoggerFactory.getLogger(LoginController.class);
-	
-	@RequestMapping(value = "SignInPage")
-	public String goTestLogin() {
-		
-		return "login-test.html";
-	}
-	
-	@RequestMapping(value = "authenticateTheUser", method=RequestMethod.POST)
-	public String doTestLogin(@RequestParam("user_id") String userId, @RequestParam("user_password") String userPwd) {
 
-		logger.info("*** test id : " + userId);
-		logger.info("*** test pwd : " + userPwd);
-		
-		return "login-test.html";
-	}
+    private Logger logger = LoggerFactory.getLogger(LoginController.class);
+
+    @RequestMapping(value = "SignInPage")
+    public String goTestLogin() {
+
+        return "login-test.html";
+    }
+
+//    @RequestMapping(value = "signin", method = RequestMethod.POST)
+//    public String doTestLogin(@Valid @RequestBody LoginRequest loginRequest) {
+//        Authentication authentication = this.authenticationManager.authenticate(
+//                new UsernamePasswordAuthenticationToken(
+//                        loginRequest.getUsername(),
+//                        loginRequest.getPassword()
+//                )
+//        );
+//        SecurityContextHolder.getContext().setAuthentication(authentication);
+//        String jwt = this.tokenProvider.makeToken(authentication);
+//        // jwt 를 main page에 넘기기
+//        return "redirect:/..";
+//    }
 }
