bfb630c3802362199d98a8613bb6b40e03e7b054
kihwankim <clack2933@gmail.com>
2020-06-24 01:08:32 +0900

    fix SecurityConfig for adding handler

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,45 +1,69 @@
 package com.example.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//    @Autowired // jpa data base connection
-//    private DataSource dataSource;
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.jdbcAuthentication().dataSource(this.dataSource);
-        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+    @Autowired
+    @Qualifier("loginService")
+    private UserDetailsService userDetailsService; // register servic class
 
-        auth.inMemoryAuthentication()
-                .withUser(userBuilder.username("abc").password("abc").roles("STUDENT", "ADMIN"))
-                .withUser(userBuilder.username("bcd").password("bcd").roles("STUDENT"))
-                .withUser(userBuilder.username("cde").password("cde").roles("STUDENT"));
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(this.userDetailsService).passwordEncoder(this.bCryptPasswordEncoder());
+    }
+
+    @Bean
+    public AuthenticationManager customAuthenticationManager() throws Exception {
+        return authenticationManager();
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-//                .csrf()
-//                .disable()
+                .csrf()
+                .disable()
                 .authorizeRequests()
                 .antMatchers("/")
                 .permitAll()
-                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/admin/**")
+                .hasRole("ADMIN")
+                .anyRequest()
+                .authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login/SignInPage")
-                .loginProcessingUrl("/login/authenticateTheUser")
+                .successHandler((httpServletRequest, httpServletResponse, authentication) -> {
+                    RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+                    redirectStrategy.sendRedirect(httpServletRequest, httpServletResponse, "/");
+                })
+                .failureHandler((httpServletRequest, httpServletResponse, e) -> {
+                    httpServletRequest.setAttribute("username", httpServletRequest.getParameter("username"));
+                    httpServletRequest.getRequestDispatcher("/login/SignInPage").forward(httpServletRequest, httpServletResponse);
+                })
                 .permitAll()
                 .and()
-                .logout().permitAll()
+                .logout()
+                .permitAll()
                 .and()
                 .exceptionHandling()
                 .accessDeniedPage("/accessDenied");
