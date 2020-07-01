77572ba6f98fcdd3a3d7ef9a910aa081b1a5db44
LeeJiHyeong <mky1168@naver.com>
2020-07-01 18:23:22 +0900

    add free_board domain and repository

--- src/main/java/com/example/demo/board/domain/FreeBoard.java
+++ src/main/java/com/example/demo/board/domain/FreeBoard.java
@@ -1,11 +1,10 @@
 package com.example.demo.board.domain;
 
+import java.util.List;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-
-import com.example.demo.login.domain.DateAudit;
-import com.sun.xml.bind.v2.TODO;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -14,6 +13,42 @@ import lombok.Setter;
 @Setter
 @Entity
 @Table(name = "free_board")
-public class FreeBoard extends DateAudit {
-	
+public class FreeBoard {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@NotBlank
+	@Size(max = 40)
+	@Column(name = "title")
+	private String title;
+
+	@Column(name = "writer_id")
+	private long writerId;
+
+	@NotBlank
+	@Size(max = 20)
+	@Column(name = "writer_name")
+	private String writerName;
+
+	@NotBlank
+	@Column(name = "content", columnDefinition = "TEXT")
+	private String content;
+
+	@OneToMany
+	@JoinColumn(name = "free_board_id")
+	private List<FreeBoardFile> freeBoardFile;
+
+	public FreeBoard() {
+
+	}
+
+	public FreeBoard(String title, long writerId, String writerName, String content) {
+		this.title = title;
+		this.writerId = writerId;
+		this.writerName = writerName;
+		this.content = content;
+	}
 }
--- src/main/java/com/example/demo/board/reposiroty/FreeBoardRepository.java
+++ src/main/java/com/example/demo/board/reposiroty/FreeBoardRepository.java
@@ -4,9 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.demo.board.domain.FreeBoard;
 
-public interface FreeBoardRepository extends JpaRepository<FreeBoard, Long>{
-	
+@Repository
+public interface FreeBoardRepository extends JpaRepository<FreeBoard, Long> {
+
+	public Optional<FreeBoard> findById(Long id);
+
+	public List<FreeBoard> findByTitleLike(String title);
+
+	public List<FreeBoard> findByWriterName(String writerName);
 }
