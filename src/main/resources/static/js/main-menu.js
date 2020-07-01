373125ba715c273036b898a567130821e2afa097
KwonOhBeom <xpzmfhf@gmail.com>
2020-07-02 00:54:07 +0900

    Header and Footer modification / Create login page CSS

--- src/main/resources/static/js/main-menu.js
+++ src/main/resources/static/js/main-menu.js
@@ -6,25 +6,29 @@ var mainMenu = (function() {
 		current = -1;
 
 	function init() {
-		$menuItems.on( 'click', open );
+		$menuItems.on( 'mouseenter', open );
 		$listItems.on( 'click', function( event ) { event.stopPropagation(); } );
 	}
 
 	function open( event ) {
-
 		var $item = $( event.currentTarget ).parent( 'li.has-submenu' ),
 			idx = $item.index();
+		
+		if( current !== idx ){
+			$listItems.removeClass('mainmenu-open');
+		}
+		
 		if($item.length != 0){
 			if( current !== -1 ) {
 				$listItems.eq( current ).removeClass( 'mainmenu-open' );
 			}
 
 			if( current === idx ) {
-				$item.removeClass( 'mainmenu-open' );
+				$item.addClass( 'mainmenu-open' ).on( 'mouseleave', allClose );
 				current = -1;
 			}
 			else {
-				$item.addClass( 'mainmenu-open' );
+				$item.addClass( 'mainmenu-open' ).on( 'mouseleave', allClose );
 				current = idx;
 				$body.off( 'click' ).on( 'click', close );
 			}
@@ -36,6 +40,10 @@ var mainMenu = (function() {
 	function close( event ) {
 		$listItems.eq( current ).removeClass( 'mainmenu-open' );
 		current = -1;
+	}
+	
+	function allClose (event) {
+		$listItems.removeClass('mainmenu-open');
 	}
 
 	return { init : init };
