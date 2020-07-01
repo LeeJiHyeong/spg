dc0ab7345ee1944a453f15c6c84aa5eac9777408
KwonOhBeom <xpzmfhf@gmail.com>
2020-07-02 00:24:46 +0900

    test

--- src/main/java/com/example/demo/main/controller/MainController.java
+++ src/main/java/com/example/demo/main/controller/MainController.java
@@ -5,18 +5,43 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/Main")
 public class MainController {
 
 	@RequestMapping(value = "/")
 	public ModelAndView goMain() {
-		
 		ModelAndView mav = new ModelAndView();
-		mav.setViewName("index");
+		mav.setViewName("index.html");
 		return mav;
 	}
 
 	@RequestMapping(value = "/accessDenied")
 	public String accessDenied(){
+		return "access-deny.html";
+	}
+	
+	@RequestMapping(value = "gallery")
+	public String gallery(){
+		return "/board/gallery.html";
+	}
+	
+	@RequestMapping(value = "freeBoard")
+	public String freeBoard(){
+		return "/board/free_board.html";
+	}
+	
+	@RequestMapping(value = "eduBoard")
+	public String eduBoard(){
+		return "/board/education_board.html";
+	}
+	
+	@RequestMapping(value = "/test1")
+	public String test1(){
+		return "access-deny.html";
+	}
+	
+	@RequestMapping(value = "/test2")
+	public String test2(){
 		return "access-deny.html";
 	}
 }
