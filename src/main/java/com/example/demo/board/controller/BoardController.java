7818a57a06ce28c6141d1c26cdceb3b8122cf8bd
LeeJiHyeong <mky1168@naver.com>
2020-07-07 17:02:23 +0900

    add file upload process in freeBoard

--- src/main/java/com/example/demo/board/controller/BoardController.java
+++ src/main/java/com/example/demo/board/controller/BoardController.java
@@ -1,10 +1,15 @@
 package com.example.demo.board.controller;
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.InputStream;
 import java.util.List;
+import java.util.UUID;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
+import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +18,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.method.annotation.ModelMethodProcessor;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.demo.board.service.FreeBoardFileService;
 import com.example.demo.board.service.FreeBoardService;
 import com.example.demo.login.domain.User;
 import com.example.demo.login.service.UserService;
@@ -29,6 +37,9 @@ public class BoardController {
 	
 	@Autowired
 	private FreeBoardService freeBoardService;
+	
+	@Autowired
+	private FreeBoardFileService freeBoardFileService;
 	
 	@Autowired
 	private UserService userService;
@@ -68,11 +79,35 @@ public class BoardController {
 	}
 	
 	@PostMapping(value = "freeBoard/doWrite")
-	public String doWrite(@ModelAttribute @Valid FreeBoard freeBoard) {
+	public String doWrite(@ModelAttribute @Valid FreeBoard freeBoard
+							,@RequestParam("upload") MultipartFile uploadFile) {
 		
 		System.out.println(">>> 게시글 저장 프로세스");
 		
-		this.freeBoardService.save(freeBoard);
+		//TODO:: 이지형, file table 수정시 파일업로드 프로세스 service로 옮겨야함
+		
+		String ordinaryFileName = uploadFile.getOriginalFilename();
+		
+		if (!ordinaryFileName.equals("")) {
+			String storeFileName = UUID.randomUUID().toString();
+			String fileSize = Long.toString(uploadFile.getSize());
+			String fileExt = ordinaryFileName.substring(ordinaryFileName.lastIndexOf(".") + 1);
+			
+			try {
+				File file = new File("C:/spg_file/" + storeFileName);
+				uploadFile.transferTo(file);
+				
+				Long freeBoardId = this.freeBoardService.save(freeBoard).getId();
+				FreeBoardFile freeBoardfile = new FreeBoardFile(storeFileName, ordinaryFileName, freeBoardId.longValue());
+				this.freeBoardFileService.save(freeBoardfile);
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		else {
+			this.freeBoardService.save(freeBoard);
+		}
 		
 		return "redirect:/board/freeBoard";
 	}
