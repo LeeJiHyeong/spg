fa3c55a014aaa559927d4015607e15f9dd0fac67
kihwankim <clack2933@gmail.com>
2020-07-11 22:38:04 +0900

    add error message expression code and tag

--- src/main/java/com/example/demo/login/controller/RegisterController.java
+++ src/main/java/com/example/demo/login/controller/RegisterController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -50,5 +51,10 @@ public class RegisterController {
         this.logger.info("Successfully created user: " + username);
 
         return "redirect:/login/signInPage";
+    }
+
+    @GetMapping("/registration")
+    public String redirectRegisterPage() {
+        return "redirect:/register/signUpPage";
     }
 }
