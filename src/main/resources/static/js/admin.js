3e0b28e35c792af4c9b42bf8d89382cd2719b153
LeeJiHyeong <mky1168@naver.com>
2020-09-03 19:52:22 +0900

    Add deafult check feature

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -1,40 +1,61 @@
 function onClickModifyRoleEventHandler(id) {
-	document.getElementById(id).setAttribute("action", "/admin/doModifyUserRole");
-	document.getElementById(id).submit();
+   document.getElementById(id).setAttribute("action", "/admin/doModifyUserRole");
+   document.getElementById(id).submit();
 }
 
 function onClickUrlEventHandler(id) {
-	document.getElementById(id).setAttribute("action", "/admin/doDeleteUser");
-	document.getElementById(id).submit();
+   document.getElementById(id).setAttribute("action", "/admin/doDeleteUser");
+   document.getElementById(id).submit();
 }
 
 function onClickCheckBoxEvnetHandler(id){
-	const checkboxList = document.getElementsByClassName(id);
-	let isUnauth = false;
-	for(let index = 0; index < checkboxList.length; index++){
-		const checkboxObj = checkboxList[index];
-		if(checkboxObj.value.trim() == "ROLE_UNAUTH" && checkboxObj.checked == true){
-			isUnauth = true;
-		}
-	}
-	
-	if(isUnauth){
-		for(let index = 0; index < checkboxList.length; index++){
-			const checkboxObj = checkboxList[index];
-			if(checkboxObj.value.trim() != "ROLE_UNAUTH"){
-				checkboxObj.checked = false;
-				checkboxObj.setAttribute("disabled", true);
-			}
-		}	
-	} else {
-		for(let index = 0; index < checkboxList.length; index++){
-			const checkboxObj = checkboxList[index];
-			if(checkboxObj.disabled){
-				checkboxObj.disabled = false;
-				if(checkboxObj.value.trim() == "ROLE_STUDENT"){
-					checkboxObj.checked = true;
-				}
-			}
-		}
-	}
+   const checkboxList = document.getElementsByClassName(id);
+   let isUnauth = false;
+   let isChecked = true;
+   
+   
+   
+   for(let index = 0; index < checkboxList.length; index++){
+      const checkboxObj = checkboxList[index];
+      if(checkboxObj.value.trim() == "ROLE_UNAUTH" && checkboxObj.checked == true){
+         isUnauth = true;
+      }
+      
+      if(checkboxObj.checked == true){
+         isChecked = false;
+      }
+      
+   }
+   
+   if(isChecked){
+      alert("최소 하나의 권한 설정 필요")
+      for(let index = 0; index < checkboxList.length; index++){
+         const checkboxObj = checkboxList[index];
+         checkboxObj.checked = false;
+         checkboxObj.disabled = false;
+         if(checkboxObj.value.trim() == "ROLE_STUDENT"){
+            checkboxObj.checked = true;
+         }
+      }   
+   }
+   
+   if(isUnauth){
+      for(let index = 0; index < checkboxList.length; index++){
+         const checkboxObj = checkboxList[index];
+         if(checkboxObj.value.trim() != "ROLE_UNAUTH"){
+            checkboxObj.checked = false;
+            checkboxObj.setAttribute("disabled", true);
+         }
+      }   
+   } else {
+      for(let index = 0; index < checkboxList.length; index++){
+         const checkboxObj = checkboxList[index];
+         if(checkboxObj.disabled){
+            checkboxObj.disabled = false;
+            if(checkboxObj.value.trim() == "ROLE_STUDENT"){
+               checkboxObj.checked = true;
+            }
+         }
+      }
+   }
 }
